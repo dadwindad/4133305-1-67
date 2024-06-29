731f3b1ed8e4e12469f1045b3ca0566d5dbb49ce
dadwindad <vongpramate@gmail.com>
2024-06-30 05:42:12 +0700

    del

--- tml/app/api/user/route.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
-
-export async function GET(){
-    const allUsers = await prisma.user.findMany()
-
-    return Response.json(allUsers)
-}
